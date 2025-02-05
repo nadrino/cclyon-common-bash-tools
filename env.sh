625d10f4de7469587a9e58372afe1afabf923a1d
Adrien Blanchet <adrien.blanchet@live.fr>
2025-02-05 15:51:54 +0100

    up

--- env.sh
+++ env.sh
@@ -8,16 +8,15 @@ export COMMON_INSTALL_DIR=$COMMON_WORK_DIR/install
 
 # Detect the OS and version
 if [ -f /etc/os-release ]; then
-    . /etc/os-release
-    OS_NAME=$ID
-    OS_VERSION=$VERSION_ID
+  . /etc/os-release
+  OS_NAME=$ID
+  OS_VERSION=$VERSION_ID
 else
-    echo "Cannot determine the OS version."
+  echo "Cannot determine the OS version."
 fi
 
 
-function link_t2k_common_soft()
-{
+function t2k_link_common_soft(){
   echo -e "Setting up T2K libs..."
 
   local current_path=${PWD}
@@ -25,33 +24,31 @@ function link_t2k_common_soft()
   echo -e "Linking libs in $COMMON_INSTALL_DIR"
   for dir in $COMMON_INSTALL_DIR/*/     # list directories in the form "/tmp/dirname/"
   do
-      dir=${dir%*/}      # remove the trailing "/"
-      sub_folder=${dir##*/} # print everything after the final "/"
+    dir=${dir%*/}      # remove the trailing "/"
+    sub_folder=${dir##*/} # print everything after the final "/"
 
-      # Skip directories that start with "root-"
-      if [[ $sub_folder == root-* ]]; then
-          continue
-      fi
+    # Skip directories that start with "root-"
+    if [[ $sub_folder == root-* ]]; then
+      continue
+    fi
 
-      export PATH="$COMMON_INSTALL_DIR/$sub_folder/bin:$PATH"
-      export LD_LIBRARY_PATH="$COMMON_INSTALL_DIR/$sub_folder/lib:$LD_LIBRARY_PATH"
-      echo "   ├─ Adding : $sub_folder"
+    export PATH="$COMMON_INSTALL_DIR/$sub_folder/bin:$PATH"
+    export LD_LIBRARY_PATH="$COMMON_INSTALL_DIR/$sub_folder/lib:$LD_LIBRARY_PATH"
+    echo "   ├─ Adding : $sub_folder"
   done
 
   builtin cd $current_path
 
-  setup_root v6-32-04
+  t2k_setup_root v6-32-04
 
   # cleanup in case it has been called multiple times
-  remove_duplicate_paths PATH
-  remove_duplicate_paths LD_LIBRARY_PATH
+  t2k_remove_duplicate_paths PATH
+  t2k_remove_duplicate_paths LD_LIBRARY_PATH
 
   echo "T2K common software has been setup."
-}; export -f link_t2k_common_soft
+}; export -f t2k_link_common_soft
 
-
-
-function setup_root(){
+function t2k_setup_root(){
 
   if [ -z "$1" ]; then
     echo "Version not specified, printing out the available versions..."
@@ -86,31 +83,32 @@ function setup_root(){
     echo "   ├─ ROOT Version : $(root-config --version)"
   fi
 
-}; export -f setup_root
+}; export -f t2k_setup_root
 
-function remove_duplicate_paths() {
-    local var_name="$1"  # Name of the variable to modify
-    local var_value="${!var_name}"  # Get the value of the variable
-    local result=""
-    local path
+function t2k_remove_duplicate_paths() {
+  local var_name="$1"  # Name of the variable to modify
+  local var_value="${!var_name}"  # Get the value of the variable
+  local result=""
+  local path
 
-    # Split the colon-separated string and process each path
-    IFS=":" read -ra paths <<< "$var_value"
-    
-    # Iterate over the paths and add them if they are not already in the result
-    for path in "${paths[@]}"; do
-        if [[ ":$result:" != *":$path:"* ]]; then
-            # Add the path to the result if it's not a duplicate
-            if [[ -n "$result" ]]; then
-                result="$result:$path"
-            else
-                result="$path"
-            fi
-        fi
-    done
-    
-    # Set the new value back to the original variable using eval
-    eval "$var_name=\"$result\""
-}; export -f remove_duplicate_paths
+  # Split the colon-separated string and process each path
+  IFS=":" read -ra paths <<< "$var_value"
 
-link_t2k_common_soft
+  # Iterate over the paths and add them if they are not already in the result
+  for path in "${paths[@]}"; do
+    if [[ ":$result:" != *":$path:"* ]]; then
+      # Add the path to the result if it's not a duplicate
+      if [[ -n "$result" ]]; then
+        result="$result:$path"
+      else
+        result="$path"
+      fi
+    fi
+  done
+
+  # Set the new value back to the original variable using eval
+  eval "$var_name=\"$result\""
+}; export -f t2k_remove_duplicate_paths
+
+# execute
+t2k_link_common_soft
